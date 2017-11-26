55d27d5ee4fd5b9a58dca9acb64896a93015464e
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-11-26 04:45:49 -0200

    Aplicando regra para ver somente dispositivo que o usuario com o token pertence

--- controllers/DispositivoController.php
+++ controllers/DispositivoController.php
@@ -2,9 +2,31 @@
 
 namespace app\controllers;
 
+use app\models\Dispositivo;
+use yii\data\ActiveDataProvider;
+
 class DispositivoController extends AuthController
 {
   public $modelClass = 'app\models\Dispositivo';
+
+  public function actions()
+  {
+      $actions = parent::actions();
+  
+      // customize the data provider preparation with the "prepareDataProvider()" method
+      $actions['index']['prepareDataProvider'] = [$this, 'prepareDataProvider'];
+  
+      return $actions;
+  }
+  public function prepareDataProvider()
+  {
+    // prepare and return a data provider for the "index" action
+    $provider = new ActiveDataProvider([
+        'query' => Dispositivo::find()->where(['id_administrador' => \Yii::$app->user->identity->id]),
+    ]);
+    
+    return $provider;
+  }
 
   public function checkAccess($action, $model = null, $params = [])
   {
