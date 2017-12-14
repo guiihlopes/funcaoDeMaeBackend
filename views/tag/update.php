a067f8e31a3b4fb1e76aa232ed94815a42903662
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-12-14 01:21:02 -0200

    Atualizando tela de update da tag

--- views/tag/update.php
+++ views/tag/update.php
@@ -5,14 +5,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Tag */
 
-$this->title = 'Atualizar tag: ' . $model->apelidoTag;
+$this->title = 'Atualizar tag';
 $this->params['breadcrumbs'][] = ['label' => 'Tags', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->idTag, 'url' => ['view', 'id' => $model->idTag]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="tag-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="font-25">Tag: <?= $model->apelidoTag ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
