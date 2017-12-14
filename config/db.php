37ae1ee6eebc1c55a678180130fb4700727c5afe
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-12-14 00:33:10 -0200

    Voltando pro sqlser

--- config/db.php
+++ config/db.php
@@ -2,8 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    // 'dsn' => 'sqlsrv:Server=funcaodemae.database.windows.net;Database=funcao_de_mae;',
-    'dsn' => 'dblib:host=funcaodemae.database.windows.net;dbname=funcao_de_mae',
+    'dsn' => 'sqlsrv:Server=funcaodemae.database.windows.net;Database=funcao_de_mae;',
     'username' => 'funcaodemae@funcaodemae',
     'password' => 'Lorena84',
     'charset' => 'utf8',
