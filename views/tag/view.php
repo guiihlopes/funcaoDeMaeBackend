bbcfc7ce2287405dec2399e618fdc818a8985369
Guilherme Lopes <guilherme.lopes@vagas.com.br>
2017-12-14 01:12:51 -0200

    Deixando em portugues botoes da tag

--- views/tag/view.php
+++ views/tag/view.php
@@ -15,8 +15,8 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->idTag], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->idTag], [
+        <?= Html::a('Atualizar', ['update', 'id' => $model->idTag], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Deletar', ['delete', 'id' => $model->idTag], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
